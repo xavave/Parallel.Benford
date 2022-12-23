03cf5cb4c7823b4cda5120af26e3d65d672c641c
Xavier AVERBOUCH <xavave@gmail.com>
2022-12-23 16:47:52 +0100

    implemented parallel foreach for calculations

--- Benford/Program.cs
+++ Benford/Program.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+
 using SixLabors.ImageSharp;
 using SixLabors.ImageSharp.PixelFormats;
 
@@ -35,8 +37,8 @@ internal class Program
 
         for (var x = 0; x < 9; x++)
         {
-            var temp = (decimal)(package.Data[x]/package.Total); // Calc % of total for x
-            var ben = (decimal) Benford(x+1); // Calc Benford's value % for x
+            var temp = (decimal)(package.Data[x] / package.Total); // Calc % of total for x
+            var ben = (decimal)Benford(x + 1); // Calc Benford's value % for x
             var deviation = ben - temp;
 
             // GenerateOutput output that also includes standard deviation for Benford's value versus observed value
@@ -78,62 +80,36 @@ internal class Program
         {
             img.ProcessPixelRows(accessor =>
             {
+
                 for (var y = 0; y < accessor.Height; y++)
                 {
-                    var pixelRow = accessor.GetRowSpan(y);
+                    Span<Rgba32> pixelRow = accessor.GetRowSpan(y);
 
                     // pixelRow.Length has the same value as accessor.Width,
                     // but using pixelRow.Length allows the JIT to optimize away bounds checks:
-                    for (var x = 0; x < pixelRow.Length; x++)
-                    {
-                        // Get a reference to the pixel at position x
-                        ref var pixel = ref pixelRow[x];
+                    Parallel.ForEach<Rgba32>(pixelRow.ToArray(), (pixel, state, sum) =>
+                   {
 
-                        // Convert pixel data into 32-bit RGBA value
-                        var _string = ((uint) ((pixel.R > 0 ? pixel.R : 1) * (pixel.G > 0 ? pixel.G : 1) *
-                                               (pixel.B > 0 ? pixel.B : 1) * (pixel.A > 0 ? pixel.A : 1))).ToString();
+                       // Get a reference to the pixel at position x
 
-                        // Get the first digit of the value
-                        var val = _string.Substring(0, 1);
+                       // Convert pixel data into 32-bit RGBA value
+                       var _string = ((uint)((pixel.R > 0 ? pixel.R : 1) * (pixel.G > 0 ? pixel.G : 1) *
+                                              (pixel.B > 0 ? pixel.B : 1) * (pixel.A > 0 ? pixel.A : 1))).ToString();
 
-                        //if (file.Contains("santa"))
-                        //    await Console.Out.WriteLineAsync(_string);
+                       // Get the first digit of the value
+                       var val = _string.Substring(0, 1);
 
-                        // Increment the count for each first digit value (1-9)
-                        switch (val)
-                        {
-                            case "1":
-                                package.Data[0]++;
-                                break;
-                            case "2":
-                                package.Data[1]++;
-                                break;
-                            case "3":
-                                package.Data[2]++;
-                                break;
-                            case "4":
-                                package.Data[3]++;
-                                break;
-                            case "5":
-                                package.Data[4]++;
-                                break;
-                            case "6":
-                                package.Data[5]++;
-                                break;
-                            case "7":
-                                package.Data[6]++;
-                                break;
-                            case "8":
-                                package.Data[7]++;
-                                break;
-                            case "9":
-                                package.Data[8]++;
-                                break;
-                        }
+                       //if (file.Contains("santa"))
+                       //    await Console.Out.WriteLineAsync(_string);
 
-                        // Count total pixels evaluated
-                        package.Total++;
-                    }
+                       // Increment the count for each first digit value (1-9)
+                       package.Add(ref package.Data[int.Parse(val) - 1]);
+                       // Count total pixels evaluated
+                       package.Add(ref package.Total);
+
+                   });
+                  
+                   
                 }
             });
         }
@@ -145,7 +121,7 @@ internal class Program
     {
         var pathPrefix = "Benford";
         var depthCount = 1;
-
+        var startTime = DateTime.Now;
         while (Directory.Exists(pathPrefix) == false && depthCount < 10)
         {
             depthCount++;
@@ -166,50 +142,41 @@ internal class Program
             await Console.Out.WriteLineAsync("BENFORD ANALYSIS RUNNING...");
             await Console.Out.WriteLineAsync();
 
-            var electionFiles = new [] { "2020-Biden.txt", "2020-Trump.txt" };
-            
+            var electionFiles = new[] { "2020-Biden.txt", "2020-Trump.txt" };
+
             #region Analyze 2020 Election Results
-            
+
             foreach (var electionFile in electionFiles)
             {
                 var list = await File.ReadAllLinesAsync(Path.Combine(pathPrefix, electionFile));
                 var package = new Package();
                 var _output = new StringBuilder();
-
+                double total = 0;
                 _output.Append($"Analyzing {electionFile} ({list.Length:0,000}" + " items)...\r\n---------------------------------------------------------------------------\r\n");
-
-                foreach (var item in list.OrderBy(i => i))
+                var orderedList = list.OrderBy(i => i).ToList();
+                Parallel.ForEach(orderedList, (item, state, subtotal) =>
                 {
                     var val = item[..1];
 
-                    switch (val)
-                    {
-                        case "1": package.Data[0]++; break;
-                        case "2": package.Data[1]++; break;
-                        case "3": package.Data[2]++; break;
-                        case "4": package.Data[3]++; break;
-                        case "5": package.Data[4]++; break;
-                        case "6": package.Data[5]++; break;
-                        case "7": package.Data[6]++; break;
-                        case "8": package.Data[7]++; break;
-                        case "9": package.Data[8]++; break;
-                    }
+                    package.Add(ref package.Data[int.Parse(val) - 1]);
+                    package.Add(ref total);
 
-                    package.Total++;
-                }
 
+                });
+                package.Add(ref package.Total, total);
                 _output.Append(GenerateOutput(package));
 
                 output2.Append(_output);
 
                 await Console.Out.WriteLineAsync(_output);
             }
-            
+
             #endregion
 
             #region Analyze Images
-
-            var jpegFiles = Directory.GetFiles(Path.Combine(pathPrefix, "images"), "*.jpg");
+            var imagesPath = Path.Combine(pathPrefix, "images");
+            if (!Directory.Exists(imagesPath)) Directory.CreateDirectory(imagesPath);
+            var jpegFiles = Directory.GetFiles(imagesPath, "*.jpg");
             var tiffFiles = Directory.GetFiles(Path.Combine(pathPrefix, "images"), "*.tiff");
             var files = jpegFiles.Concat(tiffFiles);
 
@@ -227,7 +194,8 @@ internal class Program
 
                 await Console.Out.WriteLineAsync(_output);
             }
-
+            var endTime = DateTime.Now;
+            await Console.Out.WriteLineAsync($"Process Time:{endTime - startTime}");
             #endregion
 
             // Append image results to list.txt results
@@ -257,10 +225,21 @@ internal class Package
     {
         Clear();
     }
-
+    public double Add(ref double location1, double value = 1)
+    {
+        double newCurrentValue = location1; // non-volatile read, so may be stale
+        while (true)
+        {
+            double currentValue = newCurrentValue;
+            double newValue = currentValue + value;
+            newCurrentValue = Interlocked.CompareExchange(ref location1, newValue, currentValue);
+            if (newCurrentValue.Equals(currentValue)) // see "Update" below
+                return newValue;
+        }
+    }
     public double[] Data { get; set; }
 
-    public double Total { get; set; }
+    public double Total;
 
     public void Clear()
     {
